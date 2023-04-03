ade7d8c289f8266dbd33e1c5e336afada9b71302
Hunterosmun <Hunter@Osmun.net>
2023-04-03 08:04:29 -0600

    isPrime solved!

--- index.js
+++ index.js
@@ -30,11 +30,78 @@ const { PORT = '3000' } = process.env
 const port = Number.parseInt(PORT, 10)
 
 const server = new Server(socket => {
-  console.log('connected to ', socket.address())
-  socket.pipe(socket)
-  socket.on('error', error => console.log(error))
+  let buffer = Buffer.from([])
+  socket.on('data', data => {
+    buffer = Buffer.concat([buffer, data])
+    try {
+      let str = buffer.toString('utf-8')
+      while (str.includes('\n')) {
+        // split out the stuff and things
+        const [current, ...other] = str.split('\n')
+        checkIndividual(current, socket)
+        const fullStr = other.join('\n')
+        buffer = Buffer.from(fullStr)
+        str = fullStr
+      }
+    } catch (err) {
+      console.log('CCAAPPPTTAAAIIINNN!!! TELL CASSEY WE HAVE AN ERROÖR!!!', err)
+      // socket.write('CAPTAIN!! WE HAVE AN ERROR!!')
+      socket.write('malformed\n')
+    }
+  })
+  socket.on('error', error => console.log('Oopsie whoopsie', error))
 })
 
 server.listen(port).on('listening', () => {
   console.log(`TCP server listening on port ${port}`)
 })
+
+function checkIndividual (str, socket) {
+  console.log('str is: ', str)
+  let json
+  try {
+    json = JSON.parse(str)
+  } catch (err) {
+    console.log('CÄptain, we have reached errorton', err)
+    throw new Error('malformed broski')
+  }
+
+  // then check to see if it's an object with the correct properties
+  const num = json?.number
+  if (
+    json?.method !== 'isPrime' ||
+    typeof num !== 'number' ||
+    !isPlainObject(json)
+  ) {
+    console.log({ method: json?.method, num, isPlain: isPlainObject(json) })
+    throw new Error('malformed')
+  }
+
+  // lastly, check if the number is prime
+  const resp = { method: 'isPrime', prime: isPrime(num) }
+  socket.write(JSON.stringify(resp) + '\n')
+}
+
+function isPlainObject (value) {
+  return Object.prototype.toString.call(value) === '[object Object]'
+}
+
+function isPrime (value) {
+  // 1 or negative numbers cannot be prime
+  if (value < 2) return false
+  if (!Number.isInteger(value)) return false
+  const smallestDivis = Math.floor(Math.sqrt(value))
+  for (let i = 2; i <= smallestDivis; i++) {
+    if (Number.isInteger(value / i)) return false
+  }
+  return true
+}
+
+/*
+
+{"method":"isPrime","number":1}
+{"method":"isPrime","number":2}  {"method":"isPrime","number":3}
+{"method":"isPrime","number":4}
+{"method":"isPrime","number":5}
+
+*/
